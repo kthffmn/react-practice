31a0e7737dcf3f2cfc52f71a9e0e4bf2c5fa611c
Katie Hoffman <ktahoffman@gmail.com>
2016-09-07 17:07:11 -0400

    15: build a jsx live compiler

--- App.js
+++ App.js
@@ -1,46 +1,48 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+
 class App extends React.Component {
   constructor(){
     super();
-    this.state = {data: [
-        {id: 1, name: "Simon Bailey"},
-        {id: 2, name: "Thomas Burleson"},
-        {id: 3, name: "Will Button"},
-        {id: 4, name: "Kent Dodds"},
-        {id: 5, name: "Trevor Ewen"},
-        {id: 6, name: "Aaron Frost"},
-        {id: 7, name: "Joel Hooks"},
-        {id: 8, name: "Tim Kinderg"},
-        {id: 9, name: "John Lindquist"},
-        {id: 10, name: "Joe Maddalone"}
-    ]}
+    this.state = {
+      input: "/* add your jsx here*/",
+      output: "",
+      err: ""
+    }
+    this.update = this.update.bind(this);
+  }
+  update(e){
+    let code = e.target.value;
+    try{
+      this.setState({
+        output: babel.transform(code, {
+          stage: 0,
+          loose: "all"
+        }).code,
+        err: ""
+      })
+    }
+    catch(err){
+      this.setState({err: err.message})
+    }
   }
   render(){
-    let rows = this.state.data.map(person => {
-      return <PersonRow key={person.id} data={person} />
-    })
     return (
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Name</th>
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </table>
+      <div>
+        <header>{this.state.err}</header>
+        <div className="container">
+          <textarea
+            onChange={this.update}
+            defaultValue={this.state.input}
+            >
+          </textarea>
+          <pre>
+            {this.state.output}
+          </pre>
+        </div>
+      </div>
     );
   }
-}
-
-const PersonRow = (props) => {
-  return (
-    <tr>
-      <td>{props.data.id}</td>
-      <td>{props.data.name}</td>
-    </tr>
-  );
 }
 
 export default App
